4011de32362387a34d52ce83b990c7fe120a0645
Andrzej Broński <andrzej1_1@o2.pl>
2024-05-18 16:13:20 +0200

    Prepare idea for parsing arbitrary arguments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
-pub fn proof_logic() {}
+use risc0_zkvm::{default_prover, ExecutorEnv};
+
+pub fn proof_logic() {
+    let env = ExecutorEnv::builder();
+    // env.write_slice(arg1.serialize())
+    // env.write_slice(arg2.serialize())
+    // env.write_slice(arg3.serialize())
+
+    // Obtain the default prover.
+    let prover = default_prover();
+
+    // Produce a receipt by proving the specified ELF binary.
+    // let receipt = prover.prove(env, MULTIPLY_ELF_BYTES).unwrap().receipt;
+}
